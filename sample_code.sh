066b3f88c3d26b6f88c3d1224b2ee150200261f7
ravi377 <63254725+ravi377@users.noreply.github.com>
2020-04-06 22:53:16 +0530

    this is first commit
    commit changes

--- sample_code.sh
+++ sample_code.sh
@@ -2,8 +2,8 @@
 #
 
 echo "This is an Samples Script from GitHub"
-echo "This is test from Linux Server"
-echo "This is an example of a script"
+echo "This is test from Linux Server for devops"
+echo "This is an example of a devops script"
 echo "This is running in a build step and it shows changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
